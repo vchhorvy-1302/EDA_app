e6e9b2383957ad37b696dd7687770b3bf16feb09
vchhorvy-1302 <99476141+vchhorvy-1302@users.noreply.github.com>
2022-10-20 16:08:10 +0700

    Add files via upload

---
+++ my_app/pages/01_🏠_Home.py
@@ -0,0 +1,84 @@
+import base64
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+
+img=Image.open("C:\my_app/image/logo.png")
+st.set_page_config(page_title="Z1App/EDA/Home",page_icon=img)
+
+	
+
+df = px.data.iris()
+@st.experimental_memo
+def get_img_as_base64(file):
+	with open(file, "rb") as f:
+		data = f.read()
+	return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("C:\my_app/image/sidebar.png")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://static.vecteezy.com/system/resources/previews/006/861/161/original/light-blue-background-gradient-wall-design-vector.jpg");
+background-size: cover;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+
+
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+	#End Theme
+
+
+image = Image.open('C:/my_app/image/logo.png')
+st.sidebar.image(image,width=100)
+st.title("Home Page 🏠")
+st.header('Z1 Data Exploration Method')
+with st.expander(""):
+	file_format = st.radio('Select file format:', ('csv', 'excel'), key='file_format')
+	data = st.file_uploader(label = '')
+
+	use_defo = st.checkbox('Use example Dataset')
+	if use_defo:
+		data = 'c:/my_app/sample_dataset/scrap_data_sample.csv'
+
+
+	if data:
+		if file_format == 'csv' or use_defo:
+			df = pd.read_csv(data)
+		else:
+			df = pd.read_excel(data)
+ 
+st.subheader('Overview')
+st.markdown('''This is the project of data exploration based on our company method implement. Every first step in any data-intensive
+			project is understanding the available data.To this end, data scientists or data engineer spend a significant
+			part of their time carrying out data quality assessments and data exploration. In spite of this being a crucial
+			step, it usually requires repeating a series of menial tasks before the data scientist gains an understanding of
+			the dataset and can progress to the next steps in the project.In this talk, it will detail the inner workings of 
+			workflow and important parts that it will be built which automates this drudge work, enables efficient data exploration,
+			and kickstarts data science projects.''')
+
+
+st.markdown("### EDA Process")
+st.image('https://www.researchgate.net/profile/Cristina-Sousa-2/publication/342282008/figure/fig2/AS:903839578353665@1592503556728/Exploratory-Data-Analysis-EDA-steps.png', width=700)
+
---
+++ 🧰_About_us.py
@@ -0,0 +1,84 @@
+import base64
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+from streamlit_option_menu import option_menu
+
+img=Image.open("C:/my_app/image/logo.png")
+st.set_page_config(page_title="Z1App/EDA/About_us",page_icon=img,layout="wide")
+
+
+#Theme
+df = px.data.iris()
+@st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("C:\my_app/image/sidebar.png")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://static.vecteezy.com/system/resources/previews/006/861/161/original/light-blue-background-gradient-wall-design-vector.jpg");
+background-size: cover;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+
+
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+#End Theme
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+image = Image.open('C:/my_app/image/logo.png')
+
+st.image(image, width=120)
+st.sidebar.markdown("Contents")
+st.sidebar.success("Select a demo above.")
+st.write("# Welcome to Z1 Data Exploration Web Aplication 👋")
+col1, col2 = st.columns([1.5,3])
+with col1:
+    st.markdown("### **Don't Wanna Spend Much Time!**")
+    st.markdown("This Web Application can help you through your data by yourselve without code require and spend less time. If you used to spend a weeks to  go through your data by code to understand it before cleaning or analyzing.Drag and drop your data, get through all the nesessaries informations that you need to know before cleaning or analyzing insight your data.We believe that our workflow make your data more understandable.")
+with col2:
+    st.image('https://i.pinimg.com/originals/cf/94/7b/cf947b46283c10c47e3d5d945afb7053.gif', width=650)
+    
+
+
+text_style = '<p style="font-family:sans-serif; color:Purple; font-size: 20px;">Drop your dataset here to understand your data more: 👇'
+st.markdown(text_style,  unsafe_allow_html=True)
+
+
+file_format = st.radio('Select file format:', ('csv', 'excel'), key='file_format')
+data = st.file_uploader(label = '')
+
+use_defo = st.checkbox('Use example Dataset')
+if use_defo:
+    data = 'c:/my_app/sample_dataset/scrap_data_sample.csv'
+
+
+if data:
+    if file_format == 'csv' or use_defo:
+        df = pd.read_csv(data)
+    else:
+        df = pd.read_excel(data)
+    
