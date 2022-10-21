884e7b285fee5f8a8fbea9c0b9081a18381776f9
vchhorvy-1302 <99476141+vchhorvy-1302@users.noreply.github.com>
2022-10-21 14:57:08 +0700

    Update 04_📊_Dashboard.py

--- my_app/pages/04_📊_Dashboard.py
+++ my_app/pages/04_📊_Dashboard.py
@@ -69,48 +69,107 @@ with st.expander(""):
 
 
 if data is not None:
-	c1, c2=st.columns([1.9,2])
-	all_columns=df.columns
-	
-	with c1:
-		all_columns=df.columns
-		st.sidebar.markdown('### ☑️Select for box plot')
-		target_cols=st.sidebar.selectbox('Select target column',all_columns)
-		cat_column=st.sidebar.selectbox('Selecct categorical column', all_columns)
-		if st.checkbox('Boxplot'):
-			fig_bo = px.box(df, y = target_cols, color = cat_column)
-			c1.plotly_chart(fig_bo)	
-		
-	with c2:
-		st.sidebar.markdown('### ☑️Select for bar graph')
-		select_column=st.sidebar.selectbox('select column for bar chart',all_columns)
-		if st.checkbox('Bar Chart'):
-			fig_ba = px.histogram(df, x = select_column, color_discrete_sequence=['indianred'])
-			c2.plotly_chart(fig_ba)
+    def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
-if data is not None:
-	st.sidebar.markdown('### ☑️Select columns for plot map')
-	lat=st.sidebar.selectbox('Select latitude column',all_columns)
-	long=st.sidebar.selectbox('Select longitude column',all_columns)
-	df_m=pd.DataFrame({'lat':df[lat],'lon':df[long]})
-	mask=df_m.notnull()
-	df_m=df_m.where(mask).dropna()
-	if st.checkbox('Map'):
-		st.map(df_m)	
-if data is not None:
-	st.sidebar.markdown('### ☑️Select columns for  mapbox')
-	lat_column=st.sidebar.selectbox('Select lat column',all_columns)
-	lon_column=st.sidebar.selectbox('Select lon column',all_columns)
-	colors=st.sidebar.selectbox('Select column for show color', all_columns)
-	bubble=st.sidebar.selectbox('Select column for size bubble', all_columns)
-	df_m=pd.DataFrame({'latitude':df[lat_column],'longitude':df[lon_column], 'color':df[colors],'bubble':df[bubble]})
-	mask=df_m.notnull()
-	df=df_m.where(mask).dropna()
-	if st.checkbox('Mapbox'):
-		px.set_mapbox_access_token('pk.eyJ1IjoiY2hob3J2eXZvdW4xMyIsImEiOiJjbDgxNHMwb2QwNTNmM3RvNDZhNnJka2tvIn0.2lWRwiJ9t-YYHV4vFQ3cGw')
-		figMap = px.scatter_mapbox(df, lat='latitude', lon='longitude',color='color', size='bubble',
+        df = df.copy()
+
+        # Try to convert datetimes into a standard format (datetime, no timezone)
+        for col in df.columns:
+            if is_object_dtype(df[col]):
+                try:
+                    df[col] = pd.to_datetime(df[col])
+                except Exception:
+                    pass
+
+            if is_datetime64_any_dtype(df[col]):
+                df[col] = df[col].dt.tz_localize(None)
+
+        modification_container = st.container()
+
+        with modification_container:
+            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+            for column in to_filter_columns:
+                left, right = st.columns((1, 20))
+                left.write("↳")
+                # Treat columns with < 45 unique values as categorical
+                if is_categorical_dtype(df[column]) or df[column].nunique() < 30:
+                    user_cat_input = right.multiselect(
+                        f"Values for {column}",
+                        df[column].unique(),
+                        default=list(df[column].unique()),
+                    )
+                    df = df[df[column].isin(user_cat_input)]
+                elif is_numeric_dtype(df[column]):
+                    _min = float(df[column].min())
+                    _max = float(df[column].max())
+                    step = (_max - _min) / 100
+                    user_num_input = right.slider(
+                        f"Values for {column}",
+                        _min,
+                        _max,
+                        (_min, _max),
+                        step=step,
+                    )
+                    df = df[df[column].between(*user_num_input)]
+                elif is_datetime64_any_dtype(df[column]):
+                    user_date_input = right.date_input(
+                        f"Values for {column}",
+                        value=(
+                            df[column].min(),
+                            df[column].max(),
+                        ),
+                    )
+                    if len(user_date_input) == 2:
+                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                        start_date, end_date = user_date_input
+                        df = df.loc[df[column].between(start_date, end_date)]
+                else:
+                    user_text_input = right.text_input(
+                        f"Substring or regex in {column}",
+                    )
+                    if user_text_input:
+                        df = df[df[column].str.contains(user_text_input)]
+        return df
+    df=filter_dataframe(df)
+    c1, c2=st.columns([1.9,2])
+    with c1:
+        all_columns=df.columns
+        st.sidebar.markdown('### ☑️Select for box plot')
+        target_cols=st.sidebar.selectbox('Select target column',all_columns)
+        cat_column=st.sidebar.selectbox('Selecct categorical column', all_columns)
+        if st.checkbox('Boxplot'):
+            fig_bo = px.box(df, y = target_cols, color = cat_column)
+            c1.plotly_chart(fig_bo)	
+    with c2:
+        st.sidebar.markdown('### ☑️Select for bar graph')
+        select_column=st.sidebar.selectbox('select column for bar chart',all_columns)
+        if st.checkbox('Bar Chart'):
+            fig_ba = px.histogram(df, x = select_column, color_discrete_sequence=['indianred'])
+            c2.plotly_chart(fig_ba)
+
+
+    st.sidebar.markdown('### ☑️Select columns for plot map')
+    lat=st.sidebar.selectbox('Select latitude column',all_columns)
+    long=st.sidebar.selectbox('Select longitude column',all_columns)
+    df_m=pd.DataFrame({'lat':df[lat],'lon':df[long]})
+    mask=df_m.notnull()
+    df_m=df_m.where(mask).dropna()
+    if st.checkbox('Map'):
+        st.map(df_m)	
+
+    st.sidebar.markdown('### ☑️Select columns for  mapbox')
+    lat_column=st.sidebar.selectbox('Select lat column',all_columns)
+    lon_column=st.sidebar.selectbox('Select lon column',all_columns)
+    colors=st.sidebar.selectbox('Select column for show color', all_columns)
+    bubble=st.sidebar.selectbox('Select column for size bubble', all_columns)
+    df_m=pd.DataFrame({'latitude':df[lat_column],'longitude':df[lon_column], 'color':df[colors],'bubble':df[bubble]})
+    mask=df_m.notnull()
+    df=df_m.where(mask).dropna()
+    if st.checkbox('Mapbox'):
+        px.set_mapbox_access_token('pk.eyJ1IjoiY2hob3J2eXZvdW4xMyIsImEiOiJjbDgxNHMwb2QwNTNmM3RvNDZhNnJka2tvIn0.2lWRwiJ9t-YYHV4vFQ3cGw')
+        figMap = px.scatter_mapbox(df, lat='latitude', lon='longitude',color='color', size='bubble',
 									color_continuous_scale=px.colors.cyclical.IceFire)
-		st.plotly_chart(figMap, use_container_width=True)
+        st.plotly_chart(figMap, use_container_width=True)
 	
 
 
