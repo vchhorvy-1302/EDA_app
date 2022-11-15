3e631cd16d3d1978605b47e179d3d4a17b3f030b
vchhorvy-1302 <99476141+vchhorvy-1302@users.noreply.github.com>
2022-11-15 09:46:32 +0700

    Update 02_📇_Profiling.py

--- my_app/pages/02_📇_Profiling.py
+++ my_app/pages/02_📇_Profiling.py
@@ -67,102 +67,102 @@ with st.expander(""):
 
 
 
-st.subheader('Dataframe:')
-    
-st.markdown("##### View Table 📑")
-if data is not None:
-    def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-        modify = st.checkbox("Add filters")
+st.subheader('Dataset Profile:')
+with st.expander():  
+    st.markdown("##### View Table 📑")
+    if data is not None:
+        def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+            modify = st.checkbox("Add filters")
 
-        if not modify:
+            if not modify:
+                return df
+
+            df = df.copy()
+
+            # Try to convert datetimes into a standard format (datetime, no timezone)
+            for col in df.columns:
+                if is_object_dtype(df[col]):
+                    try:
+                        df[col] = pd.to_datetime(df[col])
+                    except Exception:
+                        pass
+
+                if is_datetime64_any_dtype(df[col]):
+                    df[col] = df[col].dt.tz_localize(None)
+
+            modification_container = st.container()
+
+            with modification_container:
+                to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+                for column in to_filter_columns:
+                    left, right = st.columns((1, 20))
+                    left.write("↳")
+                    # Treat columns with < 45 unique values as categorical
+                    if is_categorical_dtype(df[column]) or df[column].nunique() < 20:
+                        user_cat_input = right.multiselect(
+                            f"Values for {column}",
+                            df[column].unique(),
+                            default=list(df[column].unique()),
+                        )
+                        df = df[df[column].isin(user_cat_input)]
+                    elif is_numeric_dtype(df[column]):
+                        _min = float(df[column].min())
+                        _max = float(df[column].max())
+                        step = (_max - _min) / 100
+                        user_num_input = right.slider(
+                            f"Values for {column}",
+                            _min,
+                            _max,
+                            (_min, _max),
+                            step=step,
+                        )
+                        df = df[df[column].between(*user_num_input)]
+                    elif is_datetime64_any_dtype(df[column]):
+                        user_date_input = right.date_input(
+                            f"Values for {column}",
+                            value=(
+                                df[column].min(),
+                                df[column].max(),
+                            ),
+                        )
+                        if len(user_date_input) == 2:
+                            user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                            start_date, end_date = user_date_input
+                            df = df.loc[df[column].between(start_date, end_date)]
+                    else:
+                        user_text_input = right.text_input(
+                            f"Substring or regex in {column}",
+                        )
+                        if user_text_input:
+                            df = df[df[column].str.contains(user_text_input)]
             return df
+        st._legacy_dataframe(filter_dataframe(df))
 
-        df = df.copy()
-
-        # Try to convert datetimes into a standard format (datetime, no timezone)
-        for col in df.columns:
-            if is_object_dtype(df[col]):
-                try:
-                    df[col] = pd.to_datetime(df[col])
-                except Exception:
-                    pass
-
-            if is_datetime64_any_dtype(df[col]):
-                df[col] = df[col].dt.tz_localize(None)
-
-        modification_container = st.container()
-
-        with modification_container:
-            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-            for column in to_filter_columns:
-                left, right = st.columns((1, 20))
-                left.write("↳")
-                # Treat columns with < 45 unique values as categorical
-                if is_categorical_dtype(df[column]) or df[column].nunique() < 20:
-                    user_cat_input = right.multiselect(
-                        f"Values for {column}",
-                        df[column].unique(),
-                        default=list(df[column].unique()),
-                    )
-                    df = df[df[column].isin(user_cat_input)]
-                elif is_numeric_dtype(df[column]):
-                    _min = float(df[column].min())
-                    _max = float(df[column].max())
-                    step = (_max - _min) / 100
-                    user_num_input = right.slider(
-                        f"Values for {column}",
-                        _min,
-                        _max,
-                        (_min, _max),
-                        step=step,
-                    )
-                    df = df[df[column].between(*user_num_input)]
-                elif is_datetime64_any_dtype(df[column]):
-                    user_date_input = right.date_input(
-                        f"Values for {column}",
-                        value=(
-                            df[column].min(),
-                            df[column].max(),
-                        ),
-                    )
-                    if len(user_date_input) == 2:
-                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                        start_date, end_date = user_date_input
-                        df = df.loc[df[column].between(start_date, end_date)]
-                else:
-                    user_text_input = right.text_input(
-                        f"Substring or regex in {column}",
-                    )
-                    if user_text_input:
-                        df = df[df[column].str.contains(user_text_input)]
-        return df
-    st._legacy_dataframe(filter_dataframe(df))
-
-# Select to show shape
-    st.markdown("##### 📌Info:")
-    @st.cache
-    def info():
-        n, m = df.shape
-        st.write(f'<p style="font-size:120%,color:Blue;">Dataset contains {n} rows and {m} columns.</p>', unsafe_allow_html=True) 
-        return 
-    c1, c2, c3 = st.columns([3, 0.5, 0.5])
-    c1.dataframe(functions.df_info(df))
+    # Select to show shape
+        st.markdown("##### 📌Info:")
+        @st.cache
+        def info():
+            n, m = df.shape
+            st.write(f'<p style="font-size:120%,color:Blue;">Dataset contains {n} rows and {m} columns.</p>', unsafe_allow_html=True) 
+            return 
+        c1, c2, c3 = st.columns([3, 0.5, 0.5])
+        c1.dataframe(functions.df_info(df))
 
 
-    st.markdown('##### 🔍Null Value Information:')
-    if df.isnull().sum().sum() == 0:
-        st.write('There is not any NA value in your dataset.')
-    else:
+        st.markdown('##### 🔍Null Value Information:')
+        if df.isnull().sum().sum() == 0:
+            st.write('There is not any NA value in your dataset.')
+        else:
+            c1, c2, c3 = st.columns([4, 0.5, 0.5])
+            functions.space(2)
+            c1.dataframe(functions.df_isnull(df), width=1500)
+        
+        st.markdown('##### 📍Outlier Analysis')   
         c1, c2, c3 = st.columns([4, 0.5, 0.5])
-        functions.space(2)
-        c1.dataframe(functions.df_isnull(df), width=1500)
-    
-    st.markdown('##### 📍Outlier Analysis')   
-    c1, c2, c3 = st.columns([4, 0.5, 0.5])
-    c1.dataframe(functions.number_of_outliers(df))
-    st.markdown('##### Duplicate Value') 
-    if st.checkbox('Duplicate Value'):
-        st.write(df[df.duplicated(keep=False)])
+        c1.dataframe(functions.number_of_outliers(df)) 
+        st.markdown('##### Duplicate Rows')
+        if st.checkbox('Duplicate Value'):
+            st.write(df[df.duplicated(keep=False)])
 
 activities = ["Categorical","Numerical","Date","Boolean"]	
 with st.sidebar:
